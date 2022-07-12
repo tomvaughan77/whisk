2eb6723dc69b353efacf5c6d8e4097d43265858c
Tom Vaughan <tomvaughan77@gmail.com>
2022-07-13 00:19:29 +0100

    Added barebone tests for scheduling logic. Horribly basic atm, will be improved soon

--- pkg/schedule/schedule.go
+++ pkg/schedule/schedule.go
@@ -1,5 +1,33 @@
 package schedule
 
-func Schedule() string {
-	return "2"
+import (
+	"time"
+)
+
+type Year struct {
+	days []Day
+}
+
+type Day struct {
+	date time.Time
+	busy bool
+}
+
+type Booking struct {
+	startDate   time.Time
+	endDate     time.Time
+	daysTaken   int
+	breakLength int
+}
+
+func Schedule(calendar Year, availableDays int) ([]Booking, error) {
+	// TODO: Implement function to determine optimal holiday days
+	return []Booking{
+		{
+			startDate:   time.Now(),
+			endDate:     time.Now().AddDate(0, 0, 2),
+			daysTaken:   4,
+			breakLength: int(time.Since(time.Now().AddDate(0, 0, 2)).Hours()) / 24,
+		},
+	}, nil
 }
